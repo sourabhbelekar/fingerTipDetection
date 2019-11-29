9e6baf8e9b4c845b6c520a7accfaef4380b161bd
Sourabh Belekar <sourabh.belekar@gmail.com>
2019-11-29 14:58:19 +0100

    capture and show video frame

--- main.cpp
+++ main.cpp
@@ -7,4 +7,19 @@ using namespace cv;
 void main()
 {
 
+VideoCapture cameraStream(0);
+Mat originalFrame;
+
+if(!cameraStream.isOpened()){
+    cout<< "Cannot open Camera";
+    exit(-1);
+}
+
+while(1){
+    cameraStream >> originalFrame;
+
+    imshow("Camera Stream",originalFrame);
+    if(waitKey(30) >= 0) break;
+}
+
 }
